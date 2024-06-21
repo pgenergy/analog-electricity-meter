784003c97c1217d6027b391458138286716e650b
Simon <simon.stahmer1@uni-oldenburg.de>
2024-06-21 10:31:48 +0200

    fix: removing unused arduino ota

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <ArduinoOTA.h>
 #include <WiFi.h>
 #include <fb_gfx.h>
 
@@ -38,11 +37,6 @@
 
 SET_LOOP_TASK_STACK_SIZE(16 * 1024);  // 16KB
 
-constexpr bool USE_WEBSERVER = false;
-
-static const uint32_t UPDATE_INTERVAL = 50;
-const char *otaPassword = "energyleaf";
-
 const char *ntpServer = "pool.ntp.org";  // Recode to use TZ
 const long gmtOffset_sec = 3600;
 const int daylightOffset_sec = 0;
@@ -65,38 +59,6 @@ void setup() {
     wifiManager.autoConnect("Energyleaf_Sensor");
 
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-    ArduinoOTA
-        .onStart([]() {
-            String type;
-            if (ArduinoOTA.getCommand() == U_FLASH)
-                type = "sketch";
-            else  // U_SPIFFS
-                type = "filesystem";
-
-            Serial.println("Start updating " + type);
-            esp_camera_deinit();
-        })
-        .onEnd([]() { Serial.println("\nEnd"); })
-        .onProgress([](unsigned int progress, unsigned int total) {
-            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        })
-        .onError([](ota_error_t error) {
-            Serial.printf("Error[%u]: ", error);
-            if (error == OTA_AUTH_ERROR)
-                Serial.println("Auth Failed");
-            else if (error == OTA_BEGIN_ERROR)
-                Serial.println("Begin Failed");
-            else if (error == OTA_CONNECT_ERROR)
-                Serial.println("Connect Failed");
-            else if (error == OTA_RECEIVE_ERROR)
-                Serial.println("Receive Failed");
-            else if (error == OTA_END_ERROR)
-                Serial.println("End Failed");
-        });
-
-    ArduinoOTA.setPassword(otaPassword);
-    ArduinoOTA.begin();
 
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
@@ -177,7 +139,6 @@ void setup() {
 }
 
 void loop() {
-    ArduinoOTA.handle();
     try {
         //plan.process();
         plan.processOrdered();
